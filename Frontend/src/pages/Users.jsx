b85b61b6410b38302e08bc17fc4f2671f6b55045
Badal sah <145487733+sahbadal@users.noreply.github.com>
2025-01-28 01:23:32 +0530

    Update Users.jsx

--- Frontend/src/pages/Users.jsx
+++ Frontend/src/pages/Users.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Users = () => {
-    const [url, setUrl] = useState('http://localhost:5000/api/v1/listUsers/6794aee6ba66a9df497582eb'); // Default URL for users
+    const [url, setUrl] = useState('https://jsoncraft.onrender.com/api/v1/listUsers/6794aee6ba66a9df497582eb'); // Default URL for users
     const [users, setUsers] = useState(null);
     const [loading, setLoading] = useState(false);
 
@@ -37,7 +37,7 @@ const Users = () => {
             <div className="bg-gray-800 p-4 rounded-md mb-6">
                 <h3 className="text-xl font-semibold text-blue-500 mb-2">Example Fetch Code:</h3>
                 <pre className="bg-gray-700 p-4 rounded-md text-sm overflow-x-auto">
-                    {`fetch('http://localhost:5000/api/v1/listUsers')
+                    {`fetch('https://jsoncraft.onrender.com/api/v1/listUsers')
   .then(response => response.json())
   .then(json => console.log(json))`}
                 </pre>
